7cc30fb6d3f253725d196bacd3fd05a7b3c5f4f8
PEARHJ <0425qoguswls@naver.com>
2022-06-15 15:35:04 +0900

    로그인, 회원가입 1단계 구현완료. 회원가입 1단계에서 입력한 정보 DB USERS 테이블에 저장.

--- app/src/main/java/com/example/finalprojectvegan/LoginActivity.java
+++ app/src/main/java/com/example/finalprojectvegan/LoginActivity.java
@@ -6,27 +6,69 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button Btn_Login;
-    Button Btn_Register;
+    private EditText edit_login_id, edit_login_password;
+    private Button Btn_Login, Btn_Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        edit_login_id = findViewById(R.id.edit_login_id);
+        edit_login_password = findViewById(R.id.edit_login_password);
         Btn_Login = findViewById(R.id.Btn_Login);
+        Btn_Register = findViewById(R.id.Btn_Register);
+
+
         Btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String userID = edit_login_id.getText().toString();
+                String userPassword = edit_login_password.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                String userID = jsonObject.getString("userID");
+                                String userPassword = jsonObject.getString("userPassword");
+                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("userID", userID);
+                                intent.putExtra("userPassword", userPassword);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
             }
         });
 
-        Btn_Register = findViewById(R.id.Btn_Register);
         Btn_Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/finalprojectvegan/RegisterStep1Activity.java
+++ app/src/main/java/com/example/finalprojectvegan/RegisterStep1Activity.java
@@ -6,22 +6,68 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterStep1Activity extends AppCompatActivity {
 
-    Button Btn_RegisterFirstToSecond;
+    private EditText edit_register_name, edit_register_id, edit_register_password, edit_register_email, edit_register_phonenum;
+    private Button Btn_RegisterFirstToSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_step1);
 
+        edit_register_name = findViewById(R.id.edit_register_name);
+        edit_register_id = findViewById(R.id.edit_register_id);
+        edit_register_password = findViewById(R.id.edit_register_password);
+        edit_register_email = findViewById(R.id.edit_register_email);
+        edit_register_phonenum = findViewById(R.id.edit_register_phonenum);
+
         Btn_RegisterFirstToSecond = findViewById(R.id.Btn_RegisterFirstToSecond);
         Btn_RegisterFirstToSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegisterStep1Activity.this, RegisterStep2Activity.class);
-                startActivity(intent);
+                // EditText에 현재 입력되어 있는 값을 get(가져오기)해준다.
+                String userName = edit_register_name.getText().toString();
+                String userID = edit_register_id.getText().toString();
+                String userPassword = edit_register_password.getText().toString();
+                String userEmail = edit_register_email.getText().toString();
+                int userPhonenum = Integer.parseInt(edit_register_phonenum.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "회원 정보 1단계 등록 완료", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterStep1Activity.this, RegisterStep2Activity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "회원 정보 1단계 등록 실패", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                RegisterStep1Request registerStep1Request = new RegisterStep1Request(userName, userID, userPassword, userEmail, userPhonenum, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterStep1Activity.this);
+                queue.add(registerStep1Request);
+
             }
         });
     }
