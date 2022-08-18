4baa683d4c91b9edbb0ca06419f6b8ca3e9a6ada
3uomlkh <cw1205@duksung.ac.kr>
2022-08-18 14:40:59 +0900

    수정 Test

--- app/src/main/java/com/example/finalprojectvegan/FragOcr.java
+++ app/src/main/java/com/example/finalprojectvegan/FragOcr.java
@@ -42,5 +42,6 @@ public class FragOcr extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_frag_ocr, container, false);
+        // 수정!!!!111111111
     }
 }
